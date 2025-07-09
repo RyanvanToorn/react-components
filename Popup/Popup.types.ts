e461e53227334fe276f1dfed4fa0d86a9ab3f181
DarkLineRSA <ryanvantoorn@gmail.com>
2025-07-09 23:41:22 +0200

    Initial conversion to TS

--- Popup/Popup.types.ts
+++ Popup/Popup.types.ts
@@ -1,3 +1,36 @@
 import type { StandardProps } from "../../Common/Types/standard.types";
+import type { ReactNode } from "react";
 
-export interface PopupProps extends StandardProps {}
+export interface PopupProps extends StandardProps {
+  isOpen?: boolean;
+  title?: string;
+  subtitle?: string;
+  bodyContents?: ReactNode;
+  footerContents?: ReactNode;
+  showCloseButton?: boolean;
+}
+
+export interface PopupHeaderProps {
+  title?: string;
+  subtitle?: string;
+  onClose: () => void;
+  showCloseButton?: boolean;
+}
+
+export interface PopupContentProps {
+  bodyContents?: ReactNode;
+}
+
+export interface PopupFooterProps {
+  footerContents?: ReactNode;
+}
+
+export interface ExpandButtonProps {
+  onExpand: () => void;
+  isVisible?: boolean;
+}
+
+export interface CloseButtonProps {
+  onClose: () => void;
+  isVisible?: boolean;
+}
