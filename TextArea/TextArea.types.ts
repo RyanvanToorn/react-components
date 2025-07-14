3573cb1f4bda2a0a4348822f56c6e325aa20fdc6
DarkLineRSA <ryanvantoorn@gmail.com>
2025-07-14 16:29:12 +0200

    Initial commit

--- TextArea/TextArea.types.ts
+++ TextArea/TextArea.types.ts
@@ -1,3 +1,9 @@
 import type { StandardProps } from "../../Common/Types/standard.types";
 
-export interface AccordionProps extends StandardProps {}
+export interface TextAreaProps extends StandardProps {
+  value?: string;
+  isResizable?: boolean;
+  isEnabled?: boolean;
+  rows?: number;
+  onChange?: () => {};
+}
